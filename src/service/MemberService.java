645cc91a82dcd8bcc43015ff0ff9dd0ecb3e76e3
SonHyeJi <kuy06046@naver.com>
2024-02-10 14:36:23 +0900

    feat : 회원정보수정 서비스 구현
    회원 권한별 회원정보 수정 서비스 구현

--- src/service/MemberService.java
+++ src/service/MemberService.java
@@ -1,6 +1,14 @@
 package service;
 
+import vo.Member;
+
+import java.io.BufferedReader;
+
 public interface MemberService {
 
     void MemberRead();
+    void MemberUpdate();
+
+   void updateMemberInfo(BufferedReader sc, Member memberToUpdate, int no);
+
 }
